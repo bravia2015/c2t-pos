2d62e8f503cb7ae6592dca8d2a31fb6ef4b7d720
nguyenhuy.chung <nguyenhuy.chung@4c654b19-c13c-0410-b9c1-3755e44d297c>
2007-10-18 10:15:38 +0000

    first import

---
+++ settings.php
@@ -0,0 +1,19 @@
+<?php
+$cfg_company="C2T Store";
+$cfg_address="137/399 Ngoc Lam - Long Bien";
+$cfg_phone="0989290831";
+$cfg_email="c2tstore@gmail.com";
+$cfg_fax="";
+$cfg_website="";
+$cfg_other="";
+$cfg_server="localhost";
+$cfg_database="ppos_c2t";
+$cfg_username="ppos";
+$cfg_password="ppos91";
+$cfg_tableprefix="c2t_";
+$cfg_default_tax_rate="5";
+$cfg_currency_symbol="VND";
+$cfg_theme="serious";
+$cfg_numberForBarcode="Account/Item Number";
+$cfg_language="english.php";
+?>
